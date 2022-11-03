498d77f0dae93c77d7512aca05c4a1353cec6722
tasneemsafour <tasneemsafour@hotmail.com>
2022-11-04 01:04:19 +0200

    Add login form

--- Global.asax.cs
+++ Global.asax.cs
@@ -15,7 +15,6 @@ namespace DoctorBooking
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
